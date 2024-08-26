384d27f809fe51ff2b79c5141d0bb80f6f91e061
NoxusJr <ctt.georgejr@gmail.com>
2024-08-26 11:01:18 -0300

    Remove GIFS

--- js/screen/state/animations.js
+++ js/screen/state/animations.js
@@ -1,24 +1,30 @@
+import { changeGameToOff, changeGameToOn } from "../observer/game.js";
 import { executeAudioEffect } from "./music.js";
 export function animation(effect) {
     if (effect == "effect") {
-        runAnimation("effect", 1500);
+        // runAnimation("effect",1500)
         executeAudioEffect("effect", 2000, 1);
         return;
     }
     if (effect == "defense") {
-        runAnimation("defense", 1500);
+        // runAnimation("defense",1500)
         return;
     }
-    runAnimation(effect, 2000);
+    // runAnimation(effect, 2000)
     executeAudioEffect(effect, 2000);
 }
 function runAnimation(animation, duration) {
-    const animationDiv = document.getElementById("animation");
-    if (!animationDiv)
+    const animations = document.getElementById("animations");
+    if (!animations)
         return;
-    animationDiv.style.background = "none";
-    animationDiv.style.background = `url(./assets/gif/${animation}.gif) no-repeat center center / contain`;
+    changeGameToOff();
+    const newAnimation = document.createElement("div");
+    newAnimation.id = "animation";
+    animations.appendChild(newAnimation);
+    newAnimation.style.background = `url(./assets/gif/${animation}.gif) no-repeat center center / contain`;
     setTimeout(() => {
-        animationDiv.style.background = "none";
+        newAnimation.style.background = "none";
+        newAnimation.remove();
+        changeGameToOn();
     }, duration);
 }
--- ts/src/screen/state/animations.ts
+++ ts/src/screen/state/animations.ts
@@ -1,32 +1,42 @@
+import { changeGameToOff, changeGameToOn } from "../observer/game.js";
 import { executeAudioEffect } from "./music.js";
 
 
 export function animation(effect:string){
 
     if (effect == "effect"){
-        runAnimation("effect",1500)
+        // runAnimation("effect",1500)
         executeAudioEffect("effect", 2000, 1)
         return
     }
 
     if (effect == "defense"){
-        runAnimation("defense",1500)
+        // runAnimation("defense",1500)
         return
     }
     
-    runAnimation(effect, 2000)
+    // runAnimation(effect, 2000)
     executeAudioEffect(effect, 2000)
 }
 
 
 function runAnimation(animation:string, duration: number){
-    const animationDiv = document.getElementById("animation")
-    if (!animationDiv) return
+
+    const animations = document.getElementById("animations")
+    if (!animations) return
     
-    animationDiv.style.background = "none"
-    animationDiv.style.background = `url(./assets/gif/${animation}.gif) no-repeat center center / contain`
+    changeGameToOff()
+    
+    const newAnimation = document.createElement("div")
+    
+    newAnimation.id = "animation"
+    animations.appendChild(newAnimation)
+    
+    newAnimation.style.background = `url(./assets/gif/${animation}.gif) no-repeat center center / contain`
 
     setTimeout(()=>{
-        animationDiv.style.background = "none"
+        newAnimation.style.background = "none"
+        newAnimation.remove()
+        changeGameToOn()
     }, duration)
 }
